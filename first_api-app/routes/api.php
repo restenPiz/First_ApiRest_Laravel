c81f0ec39b77fb59207342f5b46e75918e289097
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-06-09 12:37:33 +0200

    Some updates in system

--- first_api-app/routes/api.php
+++ first_api-app/routes/api.php
@@ -5,15 +5,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\authController;
 use App\Http\Controllers\memberController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::post('/storeMember',[memberController::class, 'storeMember']);
+Route::post('/allMember',[memberController::class, 'allMember']);
